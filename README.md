041128827e689f509d10c5fbf85cdc85aaf2cf3d
IcedPeppermintTea <238953108+IcedPeppermintTea@users.noreply.github.com>
2026-08-01 19:07:40 -0700

    update web pages based on rules

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Chrome Extension that allows you to swap words in a web page
 
 - [ ] Display error message if user hits save with empty word field
 - [ ] Move functions to their own separate page / ES module
+- [ ] If the word values contain regex special characters (like ., \*, +, ?, (, ), etc.), the regex can break or behave unexpectedly. Either escape the special characters or prevent them in the input value
---
+++ js/content-script.js
@@ -0,0 +1,65 @@
+/***
+ * content script to run silently on every page
+ * apply the rules to each web page visited
+ ***/
+
+/* initial state function: read the rules from storage */
+async function init() {
+  const result = await chrome.storage.sync.get("rules");
+  const rules = result.rules || [];
+
+  if (rules.length === 0) return;
+
+  // build new array of rule objects with the new regex key-value pair
+  const regexRules = rules.map((rule) => ({
+    ...rule,
+    regex: buildRuleRegex(rule),
+  }));
+
+  // walk throughout the DOM text nodes
+  const walker = document.createTreeWalker(
+    document.body,
+    NodeFilter.SHOW_TEXT,
+    {
+      // filter and skip tags
+      acceptNode(node) {
+        const parentTag = node.parentElement?.TagName;
+        const skipTags = ["SCRIPT", "STYLE", "TEXTAREA", "INPUT", "NOSCRIPT"];
+
+        if (parentTag && skipTags.includes(parentTag)) {
+          return NodeFilter.FILTER_REJECT;
+        }
+        return NodeFilter;
+      },
+    },
+  );
+  let node;
+  while ((node = walker.nextNode())) {
+    let text = node.textContent;
+
+    regexRules.forEach((rule) => {
+      text = text.replace(rule["regex"], rule["swap"]);
+    });
+
+    if (text !== node.textContent) {
+      node.textContent = text;
+    }
+  }
+}
+
+/* helper function to escape special characters */
+function escapeRegex(str) {
+  return str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+}
+
+/* function to build a regex for each rule */
+function buildRuleRegex(rule) {
+  const safeWord = escapeRegex(rule["word"]);
+
+  const pattern = rule["isFullWord"] ? `\\b${safeWord}\\b` : safeWord;
+  const flags = rule["isCaseSensitive"] ? "g" : "gi";
+
+  return new RegExp(pattern, flags);
+}
+
+init();
